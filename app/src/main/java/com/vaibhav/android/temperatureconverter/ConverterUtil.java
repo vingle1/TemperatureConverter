06e6a4eac272f9acb63e57c6a807d0798194f422
vaibhav ingle <vingle@unomaha.edu>
2017-04-03 12:15:23 -0500

    Final working app code.

---
+++ app/src/main/java/com/vaibhav/android/temperatureconverter/ConverterUtil.java
@@ -0,0 +1,18 @@
+package com.vaibhav.android.temperatureconverter;
+
+/**
+ * Created by vaibh on 04/01/2017.
+ */
+
+public class ConverterUtil {
+    // converts to celsius
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+
+    // converts to fahrenheit
+    public static float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+}
+
